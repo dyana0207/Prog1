aa52ee02805be3ec5d0c7322194000e0e8871305
dyana0207 <90501575+dyana0207@users.noreply.github.com>
2022-05-16 22:46:41 +0200

    Update drill20.cpp

--- drill20.cpp
+++ drill20.cpp
@@ -12,25 +12,7 @@ using namespace std;
 
 template<typename C> 
 void print(const C & c){
-	ut << "Is the vector contain the 3?\n";
-
-	if(vit != vi2.end()){
-		cout << "Found at: " << distance(vi2.begin(), vit) << endl << endl;
-	}else{
-		cout<< "Not found" << endl << endl;
-	}	
-	cout << "Is the vector contain the 27?\n";
-
-	std::list<int>::iterator lit;
-	lit = std::find(li2.begin(), li2.end(), 27);
-
-	if(lit != li2.end()){
-		cout << "Found at: " << distance(li2.begin(), lit) << endl << endl;
-	}else{
-		cout<< "Not found" << endl << endl;
-	}
- 
-}for(auto & a :c)
+	for(auto & a :c)
 	{
 		cout<< a <<' ';}
 	cout<<endl;
@@ -44,25 +26,7 @@ void inc(C& c, int n){
 template<typename iter1, typename iter2>
 iter2 orai_copy(iter1 f1, iter1 e1, iter2 f2){
 	for(iter1 p =f1; p!=e1; ++p){
-	ut << "Is the vector contain the 3?\n";
-
-	if(vit != vi2.end()){
-		cout << "Found at: " << distance(vi2.begin(), vit) << endl << endl;
-	}else{
-		cout<< "Not found" << endl << endl;
-	}	
-	cout << "Is the vector contain the 27?\n";
-
-	std::list<int>::iterator lit;
-	lit = std::find(li2.begin(), li2.end(), 27);
-
-	if(lit != li2.end()){
-		cout << "Found at: " << distance(li2.begin(), lit) << endl << endl;
-	}else{
-		cout<< "Not found" << endl << endl;
-	}
- 
-}	*f2++=*p;
+ 		*f2++=*p;
 	}
 	return f2;
 }
